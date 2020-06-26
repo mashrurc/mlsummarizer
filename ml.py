f79e7cd4611939403960b9577b461e7cef5d484c
Mashrur Chowdhury <mashrurc02@gmail.com>
2020-06-26 19:49:41 -0400

    aa

--- ml.py
+++ ml.py
@@ -1,43 +1,35 @@
-# from pprint import pprint as print
-# from gensim.summarization import summarize
-
-# text = (
-#     "Thomas A. Anderson is a man living two lives. By day he is an "+
-#     "average computer programmer and by night a hacker known as "
-#     "Neo. Neo has always questioned his reality, but the truth is "+
-#     "far beyond his imagination. Neo finds himself targeted by the "+
-#     "police when he is contacted by Morpheus, a legendary computer "+
-#     "hacker branded a terrorist by the government. Morpheus awakens "+
-#     "Neo to the real world, a ravaged wasteland where most of "+
-#     "humanity have been captured by a race of machines that live "+
-#     "off of the humans' body heat and electrochemical energy and "+
-#     "who imprison their minds within an artificial reality known as "+
-#     "the Matrix. As a rebel against the machines, Neo must return to "
-#     "the Matrix and confront the agents: super-powerful computer "
-#     "programs devoted to snuffing out Neo and the entire human "
-#     "rebellion. "
-# )
-
-
-
-# print(summarize(text, split=True))
-
-
-###########################################################################################
 from wordfreq import zipf_frequency, word_frequency
 
 from gensim.summarization import keywords
 text = "Nuclear power is a clean and efficient way of boiling water to make steam, which turns turbines to produce electricity. Nuclear power plants use low-enriched uranium fuel to produce electricity through a process called fission the splitting of uranium atoms in a nuclear reactor. Uranium fuel consists of small, hard ceramic pellets that are packaged into long, vertical tubes. Bundles of this fuel are inserted into the reactor.A single uranium pellet, slightly larger than a pencil eraser, contains the same energy as a ton of coal, 3 barrels of oil, or 17,000 cubic feet of natural gas. Each uranium fuel pellet provides up to five years of heat for power generation. And because uranium is one of the world’s most abundant metals, it can provide fuel for the world’s commercial nuclear plants for generations to come. Nuclear power offers many benefits for the environment as well. Power plants don’t burn any materials so they produce no combustion by-products. Additionally, because they don’t produce greenhouse gases, nuclear plants help protect air quality and mitigate climate change. When it comes to efficiency and reliability, no other electricity source can match nuclear. Nuclear power plants can continuously generate large-scale, around-the-clock electricity for many months at a time, without interruption. Currently, nuclear energy supplies 12 percent of the world's electricity and approximately 20 percent of the energy in the United States. As of 2018, a total of 30 countries worldwide are operating 450 nuclear reactors for electricity generation. For decades, GE and Hitachi have been at the forefront of nuclear technology, setting the industry benchmark for reactor design and construction and helping utility customers operate their plants safely and reliably."
-# def processData():
-x = keywords(text,scores = True, lemmatize = True)
 
-print(sorted(x))
-text = text.replace(".", "")
-text = text.replace(",", "")
+text = text.replace(".", " ")
+text = text.replace(",", " ")
 y = set(text.split(" "))
-c = 0
-tList = []
-print(y)
+y=list(y)
+x = keywords(text, words=100,scores = True, lemmatize = True)
+x=list(x)
+print("len x", len(x), "len y", len(y))
+
+wList = []
+
+for w in y:
+    freq=round(float(zipf_frequency(w, 'en')), 3)
+    wList.append([round((10-freq)/10-0.15,2), w])
+
+print(sorted(wList))
+print(x)
+c=0
+for a in x:
+    for b in range(len(wList)):
+        if wList[b][1] in a:
+            a[1]+=wList[b][0]
+    
+
+
+print(x)
+
+# print(y)
 # for t in y:
 #     if (1 - round(float(word_frequency(t, 'en'))*10000, 3)) > 0: 
 #         numb = ((1 - round(float(word_frequency(t, 'en'))*10000, 3))+ x[c][1])/2
@@ -47,7 +39,6 @@ print(y)
 # print(sorted(tList))
 
 # print(tList)
-# print(processData())
 
 ###########################################################################################
 
