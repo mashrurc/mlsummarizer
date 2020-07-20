5bb84775a831f8a3d635f4172c84ea3eaa711b3f
miphc42 <philhc02@gmail.com>
2020-07-20 19:59:45 -0400

    django data transfer

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,7 +19,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^postendpoint', views.backgroundProcess, name='backgroundProcess'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
